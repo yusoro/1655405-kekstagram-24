0b9f38b763117b36a1633ba51968fea3efdb0761
Yulia <riajulia@yandex.ru>
2021-09-27 20:38:51 +0300

    Fixed bags in functions

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
 function getRandomIntInclusive(min, max) {
-  if (min >= 0 && min < max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else {
-    return false;
+  if (min < 0 || max < 0) { return -1; }
+  if (min > max) {
+    [min, max] = [max, min];
   }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-getRandomIntInclusive(0, 5);
+getRandomIntInclusive(2, 5);
 
 
-function getStringLength(stringLength, maxStringLength) {
-  if (stringLength <= maxStringLength) {
-    return true;
-  }
-  return false;
+function getStringLength(string, maxStringLength) {
+  return string.length <= maxStringLength;
 }
-getStringLength(10, 140);
+getStringLength('Классное фото', 140);
