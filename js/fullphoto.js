a152b319a449ac651c865a7590cbc93e857d8e0b
Yulia <riajulia@yandex.ru>
2021-10-23 16:00:43 +0300

    Заполнение окна с фото данными

--- js/fullphoto.js
+++ js/fullphoto.js
@@ -3,42 +3,47 @@ import { PHOTOS } from './description.js';
 import {addPictures} from './pictures.js';
 import {pictures} from './pictures.js';
 import {FRAGMENT} from './pictures.js';
-
+import {COMMENTS} from './comments.js';
 
 addPictures();
 pictures.appendChild(FRAGMENT);
 
 const bigPicture = document.querySelector('.big-picture');
-const commentCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
 const body = document.querySelector('body');
 const closeBigPicture = document.querySelector('.big-picture__cancel');
-const miniature = document.querySelectorAll('.picture');
+const miniature = document.querySelectorAll('.picture__img');
+const socialComments = document.querySelector('.social__comments');
+const commentFragment = document.createDocumentFragment();
 
+const urlAddress = PHOTOS.map((address) => address.url);
+const likesArray = PHOTOS.map((like) => like.likes);
+const descriptionArray = PHOTOS.map((name) => name.description);
+const commentsLength = PHOTOS.map((comment) => comment.comments.length);
 
-for (let i = 0; i < miniature.length; i++) {
+const makeComment = ({avatar, name, message}) => {
+  const commentsListItem = document.querySelector('.social__comment').cloneNode(true);
 
-  miniature[i].addEventListener('click', () => {
-    bigPicture.classList.remove('hidden');
-    commentCount.classList.add('hidden');
-    commentsLoader.classList.add('hidden');
-    body.classList.add('modal-open');
+  commentsListItem.querySelector('.social__picture').src = avatar;
+  commentsListItem.querySelector('.social__picture').alt = name;
+  commentsListItem.querySelector('.social__text').textContent = message;
 
-    document.addEventListener('keydown', (evt) => {
-      if (isEscapeKey(evt)) {
-        evt.preventDefault();
-        bigPicture.classList.add('hidden');
-        commentCount.classList.remove('hidden');
-        commentsLoader.classList.remove('hidden');
-        body.classList.remove('modal-open');
-      }
-    });
+  commentFragment.append(commentsListItem);
+};
 
+const addComments = () => {
+  COMMENTS.forEach(({avatar, name, message}) => {
+    makeComment({avatar, name, message});
   });
 
+  socialComments.innerHTML = '';
+  socialComments.append(commentFragment);
+};
+
+
+const closeFullPhoto = () => {
   closeBigPicture.addEventListener('click', () => {
     bigPicture.classList.add('hidden');
-    commentCount.classList.remove('hidden');
     commentsLoader.classList.remove('hidden');
     body.classList.remove('modal-open');
 
@@ -46,14 +51,40 @@ for (let i = 0; i < miniature.length; i++) {
       if (isEscapeKey(evt)) {
         evt.preventDefault();
         bigPicture.classList.add('hidden');
-        commentCount.classList.remove('hidden');
         commentsLoader.classList.remove('hidden');
         body.classList.remove('modal-open');
       }
     });
   });
+};
 
+const openFullPhoto = (thumbnail, url, likes, description, comments) => {
+  thumbnail.addEventListener('click', () => {
+    bigPicture.querySelector('.full-photo').src = url;
+    bigPicture.querySelector('.likes-count').textContent = likes;
+    bigPicture.querySelector('.social__caption').textContent = description;
+    bigPicture.querySelector('.comments-count').textContent = comments;
+
+    bigPicture.classList.remove('hidden');
+    commentsLoader.classList.add('hidden');
+    body.classList.add('modal-open');
+
+    document.addEventListener('keydown', (evt) => {
+      if (isEscapeKey(evt)) {
+        evt.preventDefault();
+        bigPicture.classList.add('hidden');
+        commentsLoader.classList.remove('hidden');
+        body.classList.remove('modal-open');
+      }
+    });
+
+  });
+};
+
+
+for (let i = 0; i < miniature.length; i++) {
+  openFullPhoto(miniature[i], urlAddress[i], likesArray[i], descriptionArray[i],
+    commentsLength[i]);
+  closeFullPhoto();
+  addComments();
 }
-
-
-//export {closeFullPhoto};
--- js/main.js
+++ js/main.js
@@ -6,11 +6,8 @@ import {FRAGMENT} from './pictures.js';
 import './form.js';
 import {uploadFormOpen, uploadFormClose} from './form.js';
 import './fullphoto.js';
-//import {closeFullPhoto} from './fullphoto.js';
 
 addPictures();
 pictures.appendChild(FRAGMENT);
 uploadFormOpen();
 uploadFormClose();
-//openFullPhoto();
-//closeFullPhoto();
