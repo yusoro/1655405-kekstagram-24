1eb7fa4ad6955c5d8285e097ee9691661ba441d2
Yulia <riajulia@yandex.ru>
2021-10-29 11:05:37 +0300

    Реализовано изменение масшатаба и применение эффекта к фото

--- js/form.js
+++ js/form.js
@@ -10,7 +10,36 @@ const HASHTAG_PATTERN = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 const HASHTAG_QUANTITY = 5;
 const HASHTAG_LENGTH = 20;
 let hashtagArray = [];
-
+const scaleControlBigger = document.querySelector('.scale__control--bigger');
+const scaleControlSmaller = document.querySelector('.scale__control--smaller');
+const scaleControlValue = document.querySelector('.scale__control--value');
+const imagePreview = document.querySelector('.img-upload__preview img');
+const effectsList = document.querySelector('.effects__list');
+let currentEffect = 'none';
+const effectSlider = document.querySelector('.effect-level__slider');
+const valueInput = document.querySelector('.effect-level__value');
+const radio = effectsList.querySelectorAll('.effects__radio');
+const effects = {
+  none: () => {
+    imagePreview.style.filter = 'none';
+  },
+  chrome: (value) => {
+    imagePreview.style.filter = `grayscale(${value})`;
+    console.log(`28: grayscale(${value})`);
+  },
+  sepia: (value) => {
+    imagePreview.style.filter = `sepia(${value})`;
+  },
+  marvin: (value) => {
+    imagePreview.style.filter = `invert(${value}%)`;
+  },
+  phobos: (value) => {
+    imagePreview.style.filter = `blur(${value}px)`;
+  },
+  heat: (value) => {
+    imagePreview.style.filter = `brightness(${value})`;
+  },
+};
 
 const uploadFormOpen = () => {
   imgUploadFile.addEventListener('change', () => {
@@ -109,5 +138,93 @@ hashtag.addEventListener('input', () => {
   hashtag.reportValidity();
 });
 
+const minimizePhoto = () => {
+  scaleControlSmaller.addEventListener('click', () => {
+    if (scaleControlValue.value === '100%') {
+      scaleControlValue.value = '75%';
+      imagePreview.style.transform = 'scale(0.75)';
+    } else if (scaleControlValue.value === '75%') {
+      scaleControlValue.value = '50%';
+      imagePreview.style.transform = 'scale(0.5)';
+    } else if (scaleControlValue.value === '50%') {
+      scaleControlValue.value = '25%';
+      imagePreview.style.transform = 'scale(0.25)';
+    }
+  });
+};
 
-export {uploadFormOpen, uploadFormClose};
+const maximizePhoto = () => {
+  scaleControlBigger.addEventListener('click', () => {
+    if (scaleControlValue.value === '25%') {
+      scaleControlValue.value = '50%';
+      imagePreview.style.transform = 'scale(0.5)';
+    } else if (scaleControlValue.value === '50%') {
+      scaleControlValue.value = '75%';
+      imagePreview.style.transform = 'scale(0.75)';
+    } else if (scaleControlValue.value === '75%') {
+      scaleControlValue.value = '100%';
+      imagePreview.style.transform = 'scale(1)';
+    }
+  });
+};
+
+noUiSlider.create(effectSlider, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 1,
+  step: 0.1,
+  connect: 'lower',
+
+});
+valueInput.value = 100;
+const applyEffect = () => {
+  effectsList.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('effects__preview')) {
+      currentEffect = evt.target.classList[1].replace('effects__preview--', '');
+
+      effects[currentEffect](valueInput.value);
+    }
+  });
+};
+effectSlider.noUiSlider.on('update', (values, handle) => {
+  valueInput.value = values[handle];
+  effects[currentEffect](valueInput.value);
+});
+
+
+for (let i = 0; i < radio.length; i++) {
+  radio[i].addEventListener('change', (evt) => {
+    if (evt.target.value === 'marvin') {
+      effectSlider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 100,
+        },
+        step: 1,
+        start: 100,
+      });
+    } else if (evt.target.value === 'phobos') {
+      effectSlider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 3,
+        },
+        step: 0.1,
+        start: 3,
+      });
+    } else if (evt.target.value === 'heat') {
+      effectSlider.noUiSlider.updateOptions({
+        range: {
+          min: 1,
+          max: 3,
+        },
+        step: 0.1,
+        start: 3,
+      });
+    }
+  });
+}
+
+export {uploadFormOpen, uploadFormClose, minimizePhoto, maximizePhoto, applyEffect};
--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 import {addPictures} from './pictures.js';
 import './form.js';
-import {uploadFormOpen, uploadFormClose} from './form.js';
+import {uploadFormOpen, uploadFormClose, minimizePhoto, maximizePhoto, applyEffect} from './form.js';
 import './fullphoto.js';
 
 addPictures();
 
 uploadFormOpen();
 uploadFormClose();
+minimizePhoto();
+maximizePhoto();
+applyEffect();
